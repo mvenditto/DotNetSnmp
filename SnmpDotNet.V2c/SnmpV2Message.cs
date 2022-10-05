79032cd87d4e03c902c208f3accbf5efc8108a0a
mvenditto <venditto.matteo@gmail.com>
2022-10-05 21:57:27 +0200

    add ReadFrom to SnmpV2Message and rem. shadow Pdu prop

--- SnmpDotNet.V2c/SnmpV2Message.cs
+++ SnmpDotNet.V2c/SnmpV2Message.cs
@@ -1,4 +1,5 @@
-﻿using SnmpDotNet.Common.Definitions;
+﻿using SnmpDotNet.Asn1.Serialization;
+using SnmpDotNet.Common.Definitions;
 using System.Formats.Asn1;
 using System.Text;
 
@@ -7,8 +8,6 @@ namespace SnmpDotNet.Protocol.V1
     public class SnmpV2Message: SnmpV1Message
     {
         public override ProtocolVersion ProtocolVersion => ProtocolVersion.SnmpV2c;
-
-        public Pdu Pdu { get; set; }
 
         public override void WriteTo(AsnWriter writer)
         {
@@ -24,6 +23,48 @@ namespace SnmpDotNet.Protocol.V1
                 // pdu
                 Pdu?.WriteTo(writer);
             }
+        }
+
+        public static SnmpV1Message ReadFrom(AsnReader reader)
+        {
+            var rootSeq = reader.ReadSequence();
+
+            rootSeq.TryReadInt32(out var version);
+
+            if (version != 1)
+            {
+                throw new SnmpDecodeException(
+                    $"Expected version V2c(1) found {version}");
+            }
+
+            var community = rootSeq.ReadOctetString();
+
+            var pduType = rootSeq.PeekTag();
+
+            Pdu pdu = null;
+
+            if (pduType == SnmpAsnTags.GetResponseMsg)
+            {
+                pdu = GetResponsePdu.ReadFrom(rootSeq);
+            }
+            else if (pduType == SnmpAsnTags.BulkMsg)
+            {
+                throw new NotImplementedException();
+            }
+            else if (pduType == SnmpAsnTags.InformMsg)
+            {
+                throw new NotImplementedException();
+            }
+            else if (pduType == SnmpAsnTags.GetMsg)
+            {
+                pdu = GetRequestPdu.ReadFrom(rootSeq);
+            }
+
+            return new SnmpV2Message
+            {
+                Community = Encoding.UTF8.GetString(community),
+                Pdu = pdu!
+            };
         }
     }
 }
