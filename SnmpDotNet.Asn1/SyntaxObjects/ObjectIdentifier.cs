07914636b66853e0f8d409f8b9e51a3085f6cc9e
mvenditto <venditto.matteo@gmail.com>
2022-10-05 19:46:19 +0200

    add string and byte[] explicit conversions to ObjectIdentifier

--- SnmpDotNet.Asn1/SyntaxObjects/ObjectIdentifier.cs
+++ SnmpDotNet.Asn1/SyntaxObjects/ObjectIdentifier.cs
@@ -1,17 +1,28 @@
 ﻿using SnmpDotNet.Asn1.Serialization;
 using System.Formats.Asn1;
+using System.Text;
 
 namespace SnmpDotNet.Asn1.SyntaxObjects
 {
     public readonly record struct ObjectIdentifier(string Oid) : IAsnSerializable
     {
+        public ObjectIdentifier(params byte[] octets): 
+            this(Encoding.UTF8.GetString(octets))
+        {
+
+        }
+
         public void WriteTo(AsnWriter writer)
         {
             writer.WriteObjectIdentifier(Oid);
         }
 
-        public override string ToString() => Oid;
+        public override string ToString() => Oid.ToString();
 
-        public static implicit operator string(ObjectIdentifier o) => o.Oid;
+        public static implicit operator string(ObjectIdentifier o) => o.Oid.ToString();
+
+        public static explicit operator ObjectIdentifier(string oid) => new(oid);
+
+        public static explicit operator ObjectIdentifier(byte[] octets) => new(octets);
     }
 }
