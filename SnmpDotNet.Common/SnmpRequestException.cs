e0e2aa8170c09b9fba373a89bd1159b7bc355bfe
mvenditto <venditto.matteo@gmail.com>
2022-10-05 21:36:14 +0200

    add SnmpRequestException

---
+++ SnmpDotNet.Common/SnmpRequestException.cs
@@ -0,0 +1,13 @@
+﻿using SnmpDotNet.Common.Definitions;
+
+namespace SnmpDotNet.Common
+{
+    public class SnmpRequestException : Exception
+    {
+        public SnmpRequestException(PduErrorStatus err, int errIdx)
+            :base($"The Agent responded with error {err}({(byte) err}) at index {errIdx}")
+        {
+            
+        }
+    }
+}
