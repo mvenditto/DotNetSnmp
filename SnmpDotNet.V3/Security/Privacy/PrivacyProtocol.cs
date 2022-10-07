dc2fc405750e95bbbf7971e664f86f1ece8edef6
mvenditto <venditto.matteo@gmail.com>
2022-10-07 23:44:09 +0200

    add PrivacyProtocol enum

---
+++ SnmpDotNet.V3/Security/Privacy/PrivacyProtocol.cs
@@ -0,0 +1,20 @@
+﻿namespace SnmpDotNet.V3.Security.Privacy
+{
+    public enum PrivacyProtocol
+    {
+        /// <summary>
+        /// usmNoPrivProtocol
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// usmDESPrivProtocol
+        /// </summary>
+        Des,
+
+        /// <summary>
+        /// usmAesCfb128Protocol
+        /// </summary>
+        Aes
+    }
+}
