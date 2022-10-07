53ac876707a665de52853857ff23d031d8b4dfcd
mvenditto <venditto.matteo@gmail.com>
2022-10-07 23:06:15 +0200

    add test priority order attribute

---
+++ SnmpDotNet.Test/Helpers/TestPriorityAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnmpDotNet.Test.Helpers
+{
+    namespace XUnit.Project.Attributes
+    {
+        [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+        public class TestPriorityAttribute : Attribute
+        {
+            public int Priority { get; private set; }
+
+            public TestPriorityAttribute(int priority) => Priority = priority;
+        }
+    }
+
+}
