b9b6519334007388f006a89fcf9ce101b634addd
mvenditto <venditto.matteo@gmail.com>
2022-10-07 20:48:33 +0200

    cleanup MsgFlags

--- SnmpDotNet.V3/Security/MsgFlags.cs
+++ SnmpDotNet.V3/Security/MsgFlags.cs
@@ -3,11 +3,10 @@
     [Flags]
     public enum MsgFlags : byte
     {
-        NoAuthNoPriv = 0b00000000,
-        Auth = 0b00000001,
-        Priv = 0b00000010,
-        Reportable = 0b00000100,
-        Reserved = 0b00000010,
+        NoAuthNoPriv = 0,
+        Auth         = 1,
+        Priv         = 2,
+        Reportable   = 4,
         AuthNoPriv = Auth,
         AuthPriv = Auth | Priv
     }
