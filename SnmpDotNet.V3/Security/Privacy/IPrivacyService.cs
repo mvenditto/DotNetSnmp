506f84f98d48a16c2258fe93c854aa29c769f44c
mvenditto <venditto.matteo@gmail.com>
2022-10-13 21:42:43 +0200

    add IPrivacyService

--- SnmpDotNet.V3/Security/Privacy/IPrivacyService.cs
+++ SnmpDotNet.V3/Security/Privacy/IPrivacyService.cs
@@ -3,6 +3,14 @@
     public interface IPrivacyService
     {
         public int PrivacyParametersLength { get; }
+
+        Memory<byte> DecryptScopedPdu(ReadOnlyMemory<byte> encryptedPdu, ReadOnlyMemory<byte> privParameters);
+
+        int EncryptScopedPdu(in ReadOnlyMemory<byte> scopedPdu, Span<byte> privParameters, Span<byte> encryptedScopedPdu);
+
+        void UpdateEngineBoots(int authoritativeEngineBoots);
+
+        void UpdateEngineTime(int authoritativeEngineBoots);
     }
 
 }
