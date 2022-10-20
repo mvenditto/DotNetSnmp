d505e88532ea840a0df8209f2b461bed652dd168
mvenditto <venditto.matteo@gmail.com>
2022-10-20 21:19:55 +0200

    added more comments

--- SnmpDotNet.V2c/BulkRequestPdu.cs
+++ SnmpDotNet.V2c/BulkRequestPdu.cs
@@ -8,8 +8,14 @@ namespace SnmpDotNet.Protocol.V2
     {
         public int RequestId { get; set; } = 0;
 
+        /// <summary>
+        /// how many OIDs in the request should be treated as GET request variables
+        /// </summary>
         public int NonRepeaters { get; set; } = 0;
 
+        /// <summary>
+        /// how many GET_NEXT operations to perform on each variable
+        /// </summary>
         public int MaxRepetitions { get; set; } = 0;
 
         public VarBindList? VariableBindings { get; set; }
